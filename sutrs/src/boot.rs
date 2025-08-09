8df6c0360ba31b86e5ea32dfa01bdd2eadc60104
NguyenLe1605 <lhdnguyen2002@gmail.com>
2025-08-09 14:29:15 +0700

    Add basic logging

--- sutrs/src/boot.rs
+++ sutrs/src/boot.rs
@@ -3,19 +3,40 @@
 
 use core::arch::asm;
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+use log::{debug, error, info, warn};
+use uefi::{Status, cstr16, entry, proto::console::text::Output};
 
-#[unsafe(export_name = "efi_main")]
-pub extern "C" fn main(_h: *mut core::ffi::c_void, _st: *mut core::ffi::c_void) -> usize {
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().expect("can not init uefi");
+
+    let st = match uefi::table::system_table_raw() {
+        Some(st) => st,
+        None => return Status::ABORTED,
+    };
+
+    let st = unsafe { st.as_ref() };
+    let stdout: *mut Output = st.stdout.cast();
+    let stdout = unsafe { &mut *stdout };
+
+    if let Err(err) = stdout.clear() {
+        return err.status();
+    }
+
+    if let Err(err) = stdout.output_string(cstr16!("Hello world\n")) {
+        return err.status();
+    }
+
+    info!("Initialized bootloader log.");
+    warn!("Warning on logging.");
+    error!("Error on logging.");
+    debug!("Debuggin on logging.");
+
     loop {
         unsafe {
             asm!("hlt");
         }
     }
 
-    0
+    Status::SUCCESS
 }
